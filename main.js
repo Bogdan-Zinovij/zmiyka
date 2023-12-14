76e5e43cbe089617196f2adae485d170a3782046
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-12-14 14:24:46 +0200

    Use vite for build

--- main.js
+++
@@ -1,8 +0,0 @@
-import { Snake } from "./SnakeClass.js";
-import { Game } from "./GameClass.js";
-import { Board, Target } from "./ObjectPartsClasses.js";
-
-const cvs = document.getElementById("canvas");
-const ctx = cvs.getContext("2d");
-const game = Game.getInstance();
-game.init(Snake.getInstance(), Target.getInstance(), Board.getInstance(), ctx);
---
+++ src/main.js
@@ -0,0 +1,43 @@
+import { Snake } from './SnakeClass.js';
+import { Game } from './GameClass.js';
+import { Board, Target } from './ObjectPartsClasses.js';
+
+const mount = (root) => {
+  const cvs = document.createElement('canvas');
+  cvs.id = 'canvas';
+  cvs.width = 458;
+  cvs.height = 454;
+
+  const controlsContainer = document.createElement('div');
+  const scoreElement = document.createElement('h1');
+
+  const scoreValueElement = document.createElement('span');
+  scoreValueElement.id = 'score-value';
+
+  const startButtonElement = document.createElement('button');
+  startButtonElement.id = 'button-start';
+  startButtonElement.style = 'width: 280px; padding: 30px 0; font-size: 30px';
+  startButtonElement.innerText = 'Restart Game';
+
+  const scoreTextNode = document.createElement('span');
+  scoreTextNode.innerText = 'Score:';
+  scoreElement.appendChild(scoreTextNode);
+  scoreElement.appendChild(scoreValueElement);
+
+  controlsContainer.appendChild(scoreElement);
+  controlsContainer.appendChild(startButtonElement);
+  root.appendChild(controlsContainer);
+  root.appendChild(cvs);
+
+  const ctx = cvs.getContext('2d');
+  const game = Game.getInstance();
+
+  game.init(
+    Snake.getInstance(),
+    Target.getInstance(),
+    Board.getInstance(),
+    ctx
+  );
+};
+
+export default mount;
